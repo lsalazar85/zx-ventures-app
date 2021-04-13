3e8be4ed145c6684b351932e881b0c4f8696f15d
Luis Salazar <l_salazar85@icloud.com>
2021-04-12 23:54:08 -0300

    Add app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import {MainContextProvider} from "../context/MainContext";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <MainContextProvider>
+            <Component {...pageProps} />
+        </MainContextProvider>
+    )
+}
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -59,6 +59,8 @@ const Home = () =>  {
         getData(setCategories, '/categories')
     }, []);
 
+    console.log(products)
+
   return (
     <MainWrapper>
         <GlobalStyle />
